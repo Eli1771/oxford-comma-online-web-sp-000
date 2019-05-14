2c517af7cc9eefaff09af9d9550a5d33eaf4525c
elijahmm17 <elijahmm17@gmail.com>
2019-05-14 01:48:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.size == 1 
+    puts array[0]
     return array[0]
   elsif array.size == 2 
+    puts "#{array[0]} and #{array[1]}"
     return "#{array[0]} and #{array[1]}"
   else 
     last = array.last
